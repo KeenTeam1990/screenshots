64318a14481a2ba18e70a7c6ae8750f12ee45b62
zzq <Keen_Team@163.com>
2021-09-10 13:53:36 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+## 功能点介绍
+1. 实现截取某个控件并生成image的功能。
+2. 实现MP4格式视频流的播放和暂停以及获取某一帧（视频流截图）的功能。
+3. 实现m3u8格式视频流的播放和暂停以及获取某一帧（视频流截图）的功能。
+
+## 注意事项
+1. 播放视频时需要配置info.plist中的NSAppTransportSecurity，否则可能会出现视频无法播放的问题。
+2. 在视频播放页面push到别的页面时需要停止视频的播放，否则视频会一直在播放。
+
+## 代码介绍
+1. MMScreenshotsManager中封装了三种截屏方法。
+2. MMVideoPlayer中封装了一个简单的视频播放器。
+3. 调用示例在对应的ViewController中，可供参考。
+
+## 代码运行效果预览
+### imageView截图
+![](https://vkceyugu.cdn.bspapp.com/VKCEYUGU-f92a0e71-a10b-4097-af0d-93aa474b1716/a91d08d0-ff95-40ea-9357-1d3aa8c0456a.jpeg)
+### MP4格式视频流截屏
+![](https://vkceyugu.cdn.bspapp.com/VKCEYUGU-f92a0e71-a10b-4097-af0d-93aa474b1716/cc1ee8df-4cdc-4e59-b140-66b5de6e210a.jpeg)
+### m3u8格式视频流截屏
+![](https://vkceyugu.cdn.bspapp.com/VKCEYUGU-f92a0e71-a10b-4097-af0d-93aa474b1716/51598260-54d9-49c3-8041-88bce207a4a7.jpeg)
---
+++ VideoScreenshots/MMVideom3u8CaptureViewController.m
@@ -0,0 +1,56 @@
+//
+//  MMVideom3u8CaptureViewController.m
+//  VideoScreenshots
+//
+//  Created by zhanqin on 2018/1/4.
+//  Copyright © 2018年 zhanqin. All rights reserved.
+//
+
+#import "MMVideom3u8CaptureViewController.h"
+#import "MMPushViewController.h"
+
+@interface MMVideom3u8CaptureViewController ()
+
+@end
+
+@implementation MMVideom3u8CaptureViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"m3u8截屏";
+    [self.view addSubview:self.playerView];
+    self.playerView.videoUrl = @"http://220.161.87.62:8800/hls/0/index.m3u8";
+    [self.playerView playVideo];
+    [self setupVideoButton];
+}
+
+-(void)dealloc{
+    NSLog(@"MMVideom3u8CaptureViewController dealloc");
+}
+
+-(void)screenshotsClick{
+    self.screenshotsImageView.image = [[[MMScreenshotsManager alloc] init] screenshotsm3u8WithCurrentTime:self.playerView.videPlayer.currentTime playerItemVideoOutput:self.playerView.playerOutput];
+}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    MMPushViewController * vc = [[MMPushViewController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
